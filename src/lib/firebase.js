0698c38472e663a70a31bc7863cb8cd435db0b2a
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2024-03-11 19:00:44 +0600

    changed config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,15 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBPh_xGy7LXiMku-1sR8pqVlBRRGwJveQA",
-    authDomain: "auth.revenuebnb.com",
-    projectId: "price-prediction-52b23",
-    storageBucket: "price-prediction-52b23.appspot.com",
-    messagingSenderId: "395647856066",
-    appId: "1:395647856066:web:7739297c6dbc8ea25aba5b",
-    measurementId: "G-BYJ328WFTP"
+  apiKey: "AIzaSyBSFk4V-EtoGvWanmoz3gzYhsrHVDeK3A0",
+  authDomain: "price-prediction-56917.firebaseapp.com",
+  projectId: "price-prediction-56917",
+  storageBucket: "price-prediction-56917.appspot.com",
+  messagingSenderId: "559357871168",
+  appId: "1:559357871168:web:14ddce50a675c16a7ecba5",
+  measurementId: "G-6QHYHEDLHT",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
